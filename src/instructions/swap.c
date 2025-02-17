a4a2cc3dd98d9d65bfc8eaa8bf17de897a968e8e
CRaph0705 <raphaelcochrane@hotmail.fr>
2025-02-17 14:07:16 +0100

    unused function removed

--- src/instructions/swap.c
+++ src/instructions/swap.c
@@ -6,27 +6,27 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 14:05:38 by rcochran          #+#    #+#             */
-/*   Updated: 2025/02/17 14:05:52 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/02/17 14:06:59 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		swap_conditions(t_stack *stack);
+// int		swap_conditions(t_stack *stack);
 void	ft_swap(t_stack *stack);
 void	sa(t_stack *stack_a);
 void	sb(t_stack *stack_b);
 void	ss(t_stack *stack_a, t_stack *stack_b);
 
-int	swap_conditions(t_stack *stack)
-{
-	int	len;
+// int	swap_conditions(t_stack *stack)
+// {
+// 	int	len;
 
-	len = ft_stacksize(stack);
-	if (len <= 1)
-		return (0);
-	return (1);
-}
+// 	len = ft_stacksize(stack);
+// 	if (len <= 1)
+// 		return (0);
+// 	return (1);
+// }
 
 void	ft_swap(t_stack *stack)
 {
