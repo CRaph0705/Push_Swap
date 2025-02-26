2d1081db28b0b378584d5ad32851b927500d010e
CRaph0705 <raphaelcochrane@hotmail.fr>
2025-02-27 00:29:02 +0100

    to do

--- src/chunk_sort.c
+++ src/chunk_sort.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:23:55 by rcochran          #+#    #+#             */
-/*   Updated: 2025/02/26 16:17:53 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/02/27 00:27:57 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,3 +52,17 @@ void	chunk_sort(t_stack **stack_a, t_stack **stack_b)
 // chunk 2 -> target pos entre index max / 4 et index max / 2
 // chunk 3 -> target pos entre index max / 2 et 3 index max / 4
 // chunk 4 -> target pos entre 3 index max / 4 et index max
+
+// int i : index de chunk
+// fixer une condition d'entrée dans le chunk en fonction de la target pos
+// genre if (*stack -> target_pos) < index_max / index chunk
+
+// limit_chunk1 == ptr sur node dont target_pos == 0
+// limit_chunk2 == ptr sur node dont target_pos == max_target_pos * 1/3
+// limit_chunk3 == ptr sur node dont target_pos == max_target_pos * 2/3
+
+/*//TODO >>>>>>>>>> fill chunk functions <<<<<<<<<< */
+// while stack a != NULL 
+//get_next_chunk(stack, chunk_limit);
+// -> while (*stack)->target_pos < chunk_limit->target_pos
+//get_next_chunk_node
